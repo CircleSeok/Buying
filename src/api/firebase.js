fe4fe17a4017dcf97b6cf7627c89c23f0054e321
Wonseok <dnjstjr970302@gamil.com>
2023-07-12 05:52:49 +0900

    feat/어드민 사용자

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -7,6 +7,7 @@ import {
   signOut,
   onAuthStateChanged,
 } from 'firebase/auth';
+import { getDatabase, ref, child, get } from 'firebase/database';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -19,24 +20,37 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const provider = new GoogleAuthProvider();
+const database = getDatabase(app);
 
-export async function login() {
-  return signInWithPopup(auth, provider)
-    .then((result) => {
-      const user = result.user;
-      console.log(user);
-      return user;
-    })
-    .catch(console.error);
+export function login() {
+  signInWithPopup(auth, provider).catch(console.error);
 }
 
 export async function logout() {
-  return signOut(auth).then(() => null);
+  return signOut(auth).catch(console.error);
 }
 
 export function onUserStateChange(callback) {
-  onAuthStateChanged(auth, (user) => {
-    callback(user);
+  onAuthStateChanged(auth, async (user) => {
+    //1. 사용자가 있는 경우에 (로그인한 경우)
+    const updateUser = user ? await adminUser(user) : null;
+    // console.log(user);
+    callback(updateUser);
   });
 }
 //사용자의 로그인 상태 변화를 감지하고, 상태 변화에 따라 콜백 함수를 실행합니다. onAuthStateChanged(auth, callback) 함수를 호출하여 인증 상태가 변경될 때마다 사용자 객체를 user 매개변수로 갖는 콜백 함수를 실행합니다. 이를 통해 로그인, 로그아웃, 사용자 변경 등의 상태 변화를 감지하고 적절한 동작을 수행할 수 있습니다.
+
+async function adminUser(user) {
+  //2. 사용자가 어드민 권한을 가지고 있는지 확인
+  //3. {...user, isAdmin: ture/ false}
+  return get(ref(database, 'admins')) //
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        const admins = snapshot.val();
+        // console.log(admins);
+        const isAdmin = admins.includes(user.uid);
+        return { ...user, isAdmin };
+      }
+      return user;
+    });
+}
